85fa17a1b888e775495223166f993596363b5f09
michaimmendoerfer <78658767+michaimmendoerfer@users.noreply.github.com>
2024-04-18 14:25:28 +0200

    hjk

---
+++ include/Module_Definitions.h
@@ -0,0 +1,99 @@
+#ifndef MODULE_DEFINITIONS_H
+#define MODULE_DEFINITIONS_H
+
+// Module Definition
+//#define C3_MINI
+//#define C3_MINI_PORT
+//#define C3_MINI_PORT_ADS
+//#define ESP8266_PORT
+//#define ESP8266_ADS
+#define ESP8266_4WAY_INTEGRATED
+//#define ESP32_DISPLAY_480
+//#define DISPLAY_C3_ROUND
+
+// IC-Things
+#ifdef C3_MINI
+    //#define ESP32_MODULE_1S_1V
+    //#define ESP32_MODULE_2S_1V
+    
+    #define BOOT_BUTTON 9
+    #define LED_PIN     8
+    #define LED_OFF     HIGH
+    #define LED_ON      LOW
+#endif
+
+#ifdef C3_MINI_PORT
+    #define ESP32_MODULE_4S_1V_PORT
+    //#define ESP32_MODULE_4A_1V_ADS
+
+    #define PORT_USED   1
+    #define BOOT_BUTTON 9
+    #define LED_PIN     8
+    #define LED_OFF     HIGH
+    #define LED_ON      LOW
+    #define SDA_PIN     6
+    #define SCL_PIN     7
+#endif
+
+#ifdef C3_MINI_ADS
+    //#define ESP32_MODULE_4A_1V_ADS
+
+    #define ADS_USED    1
+    #define BOOT_BUTTON 9
+    #define LED_PIN     8
+    #define LED_OFF     HIGH
+    #define LED_ON      LOW
+    #define SDA_PIN     6
+    #define SCL_PIN     7
+#endif
+
+#ifdef C3_MINI_PORT_ADS
+    #define ESP32_MODULE_4S_4A_1V_PORT
+    
+    #define PORT_USED   1
+    #define ADS_USED    1
+    #define BOOT_BUTTON 9
+    #define LED_PIN     8
+    #define LED_OFF     HIGH
+    #define LED_ON      LOW
+    #define SDA_PIN     6
+    #define SCL_PIN     7
+#endif
+
+#ifdef ESP8266_PORT
+    #define MRD_USED    1
+    #define PORT_USED   1
+    #define LED_PIN     LED_BUILTIN
+    #define LED_OFF     LOW
+    #define LED_ON      HIGH
+    #define SDA_PIN     14
+    #define SCL_PIN     12
+#endif
+
+// Jeep Battery hinten
+#ifdef ESP8266_ADS
+    #define ESP8266_MODULE_4A_1V_ADS
+    #define MRD_USED    1
+    #define ADS_USED   1
+    #define LED_PIN     LED_BUILTIN
+    #define LED_OFF     LOW
+    #define LED_ON      HIGH
+    #define SDA_PIN     4
+    #define SCL_PIN     5
+#endif
+
+// fertiges 4-Kanal ESP8266
+#ifdef ESP8266_4WAY_INTEGRATED
+    #define ESP8266_MODULE_4S_INTEGRATED
+    #define MRD_USED    1
+    #define LED_PIN     LED_BUILTIN
+    #define LED_OFF     LOW
+    #define LED_ON      HIGH
+    #define SDA_PIN     14
+    #define SCL_PIN     12
+
+    #define BOOT_BUTTON 4 
+#endif
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,96 +3,15 @@
 // DEBUG_LEVEL: 0 = nothing, 1 = only Errors, 2 = relevant changes, 3 = all
 const int DEBUG_LEVEL = 3; 
 
-// Module Definition
-//#define C3_MINI
-//#define C3_MINI_PORT
-//#define C3_MINI_PORT_ADS
-//#define ESP8266_PORT
-//#define ESP8266_ADS
-#define ESP8266_4WAY_INTEGRATED
-//#define ESP32_DISPLAY_480
-//#define DISPLAY_C3_ROUND
+#pragma region Includes
+#include <Arduino.h>
+#include "Module_Definitions.h"
 
-// IC-Things
-#ifdef C3_MINI
-    //#define ESP32_MODULE_1S_1V
-    //#define ESP32_MODULE_2S_1V
-    
-    #define BOOT_BUTTON 9
-    #define LED_PIN     8
-    #define LED_OFF     HIGH
-    #define LED_ON      LOW
-#endif
-
-#ifdef C3_MINI_PORT
-    #define ESP32_MODULE_4S_1V_PORT
-    //#define ESP32_MODULE_4A_1V_ADS
-
-    #define PORT_USED   1
-    #define BOOT_BUTTON 9
-    #define LED_PIN     8
-    #define LED_OFF     HIGH
-    #define LED_ON      LOW
-    #define SDA_PIN     6
-    #define SCL_PIN     7
-#endif
-
-#ifdef C3_MINI_ADS
-    //#define ESP32_MODULE_4A_1V_ADS
-
-    #define ADS_USED    1
-    #define BOOT_BUTTON 9
-    #define LED_PIN     8
-    #define LED_OFF     HIGH
-    #define LED_ON      LOW
-    #define SDA_PIN     6
-    #define SCL_PIN     7
-#endif
-
-#ifdef C3_MINI_PORT_ADS
-    #define ESP32_MODULE_4S_4A_1V_PORT
-    
-    #define PORT_USED   1
-    #define ADS_USED    1
-    #define BOOT_BUTTON 9
-    #define LED_PIN     8
-    #define LED_OFF     HIGH
-    #define LED_ON      LOW
-    #define SDA_PIN     6
-    #define SCL_PIN     7
-#endif
-
-#ifdef ESP8266_PORT
-    #define MRD_USED    1
-    #define PORT_USED   1
-    #define LED_PIN     LED_BUILTIN
-    #define LED_OFF     LOW
-    #define LED_ON      HIGH
-    #define SDA_PIN     14
-    #define SCL_PIN     12
-#endif
-
-#ifdef ESP8266_ADS
-    #define ESP8266_MODULE_4A_1V_ADS
-    #define MRD_USED    1
-    #define ADS_USED   1
-    #define LED_PIN     LED_BUILTIN
-    #define LED_OFF     LOW
-    #define LED_ON      HIGH
-    #define SDA_PIN     4
-    #define SCL_PIN     5
-#endif
-
-#ifdef ESP8266_4WAY_INTEGRATED
-    #define ESP8266_MODULE_4S_INTEGRATED
-    #define MRD_USED    1
-    #define LED_PIN     LED_BUILTIN
-    #define LED_OFF     LOW
-    #define LED_ON      HIGH
-    #define SDA_PIN     14
-    #define SCL_PIN     12
-
-    #define BOOT_BUTTON 4 
+#ifdef ESP32_DISPLAY_480
+    #include <esp32_smartdisplay.h>
+    #define ADS_USED  1
+    #define PORT_USED 1
+    #include <ui/ui.h>
 #endif
 
 #ifdef MRD_USED
@@ -109,17 +28,6 @@ const int DEBUG_LEVEL = 3;
 
     MultiResetDetector* mrd;
 #endif
-
-#pragma region Includes
-#include <Arduino.h>
-
-#ifdef ESP32_DISPLAY_480
-    #include <esp32_smartdisplay.h>
-    #define ADS_USED  1
-    #define PORT_USED 1
-    #include <ui/ui.h>
-#endif
-
 #pragma region I2C_BUS
 #define I2C_FREQ 400000
 #define ADS_ADDRESS  0x48
@@ -219,30 +127,32 @@ void   SetMessageLED(int Color);
 #pragma endregion Functions
 
 void InitModule()
-{
+{    
+    ////////////////////////////////////////////////////////
+    ////////////// ESP32 ///////////////////////////////////
+    ////////////////////////////////////////////////////////
     /*
     ESP32 DevKit:
-    possible Inputs:  16,17,18,19,21,22,23,       best: (ADC1:32,33,34,35,36,39)
+    possible Inputs:  16,17,18,19,21,22,23,       
     possible Outputs: 16,17,18,19,21,22,23,32,33
+    best: (ADC1:32,33,34,35,36,39)
     
     dont´t use
     ADC2 (when Wifi): 0, 2, 4, 12, 13, 14, 15, 25, 26, 27
     SPI-Flash:        6,7,8,9,10,11 
-
     */
-    
-    #ifdef ESP32_MODULE_4S_1V_NOADC_PORT   // 4-Way Switch via IOBoard with Voltage-Monitor #################################################################
-      // PCF8575 with SCL:21, SDA:22, Voltage-Monitor:35 
+
+    #ifdef ESP32_MODULE_4S_1V_NOADC_PORT    // 4-Way Switch via IOBoard with Voltage-Monitor #####################################################
       #define SWITCHES_PER_SCREEN 4
 
       //                Name        Type       Version  Address   sleep  debug  demo  pair  vMon RelayType    adc1 adc2 voltagedevier 
       Module.Setup(_ModuleName, SWITCH_4_WAY, _Version, NULL,     false, true,  true, false, -1, RELAY_NORMAL, 21,  22,     1);
 
       //                      Name     Type           ADS  IO  NULL   VpA   Vin  PeerID
-      Module.PeriphSetup(0, "SP-1", SENS_TYPE_SWITCH,  1,  0,   0,    0,    0,    0);
-      Module.PeriphSetup(1, "SP-2", SENS_TYPE_SWITCH,  1,  1,   0,    0,    0,    0);
-      Module.PeriphSetup(2, "SP-3", SENS_TYPE_SWITCH,  1,  2,   0,    0,    0,    0);
-      Module.PeriphSetup(3, "SP-4", SENS_TYPE_SWITCH,  1,  3,   0,    0,    0,    0);
+      Module.PeriphSetup(0, "SP-1", SENS_TYPE_SWITCH,  1,  P0,   0,    0,    0,    0);
+      Module.PeriphSetup(1, "SP-2", SENS_TYPE_SWITCH,  1,  P1,   0,    0,    0,    0);
+      Module.PeriphSetup(2, "SP-3", SENS_TYPE_SWITCH,  1,  P2,   0,    0,    0,    0);
+      Module.PeriphSetup(3, "SP-4", SENS_TYPE_SWITCH,  1,  P3,   0,    0,    0,    0);
       Module.PeriphSetup(4, "Volt", SENS_TYPE_VOLT,    0,  35,  0,    0,   200,   0); 
     #endif
     #ifdef ESP32_MODULE_4A_1V_ADC           // 4-way Battery-Sensor with ADC and VMon ############################################################
@@ -259,20 +169,6 @@ void InitModule()
       Module.PeriphSetup(3, "Sensor_4", SENS_TYPE_AMP,  1,    4,  2.5,  0.066,  0,    0);
       Module.PeriphSetup(4, "VMon",     SENS_TYPE_VOLT, 0,   39,   0,     0,   200,   0); 
     #endif
-    #ifdef ESP8266_MODULE_4A_1V_ADS           // 4-way Battery-Sensor with ADC and VMon ############################################################
-      // 4x acs712(30A) over ADC1115 on SCL:12, SDA:14, Voltage-Monitor:35
-      #define SWITCHES_PER_SCREEN 4
-      
-      //                Name        Type         Version  Address   sleep  debug  demo  pair  vMon RelayType    adc1 adc2 voltagedevier 
-      Module.Setup(_ModuleName, BATTERY_SENSOR, _Version, NULL,     false, true, false, false, 1,  RELAY_NORMAL, 14,  12,     1.5);
-
-      //                      Name     Type             ADS  IO   NULL   VpA   Vin  PeerID
-      Module.PeriphSetup(0, "Sensor_1", SENS_TYPE_AMP,  1,    0,  2.5,  0.066,  0,    0);
-      Module.PeriphSetup(1, "Sensor_2", SENS_TYPE_AMP,  1,    1,  2.5,  0.066,  0,    0);
-      Module.PeriphSetup(2, "Sensor_3", SENS_TYPE_AMP,  1,    2,  2.5,  0.066,  0,    0);
-      Module.PeriphSetup(3, "Sensor_4", SENS_TYPE_AMP,  1,    3,  2.5,  0.066,  0,    0);
-      Module.PeriphSetup(4, "VMon",     SENS_TYPE_VOLT, 0,   A0,   0,     0,   200,   0); 
-    #endif
     #ifdef ESP32_MODULE_2A_2S_1V_NOADC      // Mixed-Module no ADC and VMon ######################################################################
       #define SWITCHES_PER_SCREEN 2
 
@@ -287,20 +183,30 @@ void InitModule()
       Module.PeriphSetup(4, "V-Sens", SENS_TYPE_VOLT,    0,  39,   0,       0,      200,    0); 
     #endif
     
-    #ifdef ESP32_MODULE_4S_1V_PORT
-        #define SWITCHES_PER_SCREEN 2
 
-      //                Name        Type         Version  Address   sleep  debug  demo  pair  vMon   RelayType      sda       scl    voltagedevier 
-      Module.Setup(_ModuleName, SWITCH_4_WAY,   _Version, NULL,     false, true,  true, false, -1,  RELAY_NORMAL, SDA_PIN,  SCL_PIN,     1.5);
-
-      //                      Name     Type             ADS  IO    NULL     VpA      Vin  PeerID
-      Module.PeriphSetup(0, "Sw 1",   SENS_TYPE_SWITCH,   1,  P0,    0,       0,        0,    0);
-      Module.PeriphSetup(1, "Sw 2",   SENS_TYPE_SWITCH,   1,  P1,    0,       0,        0,    0);
-      Module.PeriphSetup(2, "Sw 3",   SENS_TYPE_SWITCH,   1,  P2,    0,       0,        0,    0);
-      Module.PeriphSetup(3, "Sw 4",   SENS_TYPE_SWITCH,   1,  P3,    0,       0,        0,    0);
-      Module.PeriphSetup(4, "V-Sens", SENS_TYPE_VOLT,     0,  1,     0,       0,      200,    0); 
+    ////////////////////////////////////////////////////////
+    ////////////// ESP8266 /////////////////////////////////
+    ////////////////////////////////////////////////////////
+    /*
+    possible Inputs:  
+    possible Outputs: 
+    best: 
+    
+    dont´t use
+    */
+    #ifdef ESP8266_MODULE_4A_1V_ADS           // 4-way Battery-Sensor with ADS and VMon #########################################################
+      // 4x acs712(30A) over ADC1115, Voltage-Monitor:A0
+      #define SWITCHES_PER_SCREEN 4
+      //                Name        Type         Version  Address   sleep  debug  demo  pair  vMon RelayType    adc1 adc2 voltagedevier 
+      Module.Setup(_ModuleName, BATTERY_SENSOR, _Version, NULL,     false, true, false, false, 1,  RELAY_NORMAL, 14,  12,     1.5);
+      //                      Name     Type            ADS  IO   NULL   VpA   Vin  PeerID
+      Module.PeriphSetup(0, "Amp_1",   SENS_TYPE_AMP,  1,    0,  2.5,  0.066,  0,    0);
+      Module.PeriphSetup(1, "Amp_2",   SENS_TYPE_AMP,  1,    1,  2.5,  0.066,  0,    0);
+      Module.PeriphSetup(2, "Amp_3",   SENS_TYPE_AMP,  1,    2,  2.5,  0.066,  0,    0);
+      Module.PeriphSetup(3, "Amp_4",   SENS_TYPE_AMP,  1,    3,  2.5,  0.066,  0,    0);
+      Module.PeriphSetup(4, "VMon",    SENS_TYPE_VOLT, 0,   A0,   0,     0,   200,   0); 
     #endif
-
+    
     #ifdef ESP8266_MODULE_4S_INTEGRATED
         #define SWITCHES_PER_SCREEN 4
 
